1eb2bc4f91bc34ba38f17f544fcefb9fd4b235db
Igor <igij@o2.pl>
2019-04-24 18:23:06 +0200

    lab 3_2 Formatting

--- src/main/java/edu/iis/mto/staticmock/ConfigurationLoader.java
+++ src/main/java/edu/iis/mto/staticmock/ConfigurationLoader.java
@@ -5,32 +5,35 @@ import java.io.File;
 import org.codehaus.jackson.map.ObjectMapper;
 
 public class ConfigurationLoader {
-	private static final String FILE_EXT = "json";
-	private static final String MTO_CONF_PATH = "mtoConfPath";
-	private static ConfigurationLoader instance;
 
-	private ConfigurationLoader() {
-	}
+    private static final String FILE_EXT = "json";
+    private static final String MTO_CONF_PATH = "mtoConfPath";
+    private static ConfigurationLoader instance;
 
-	public Configuration loadConfiguration()  {
-		String configurationDir = System.getProperty(MTO_CONF_PATH);
-		StringBuilder stringBuilder = new StringBuilder(configurationDir);
-		stringBuilder.append(Configuration.class.getSimpleName()).append(FILE_EXT);
-		try {
-			File file = new File(stringBuilder.toString());
-			
-			ObjectMapper mapper = new ObjectMapper();
-			return mapper.readValue(file, Configuration.class);
-		} catch (Exception e) {
-			throw new ConfigurationException(e);
-		}
-		
-	}
+    private ConfigurationLoader() {
+    }
 
-	public static ConfigurationLoader getInstance() {
-		if(instance == null) {
-			instance = new ConfigurationLoader();
-		}
-		return instance;
-	}
+    public Configuration loadConfiguration() {
+
+        String configurationDir = System.getProperty(MTO_CONF_PATH);
+        StringBuilder stringBuilder = new StringBuilder(configurationDir);
+        stringBuilder.append(Configuration.class.getSimpleName()).append(FILE_EXT);
+        try {
+            File file = new File(stringBuilder.toString());
+
+            ObjectMapper mapper = new ObjectMapper();
+            return mapper.readValue(file, Configuration.class);
+        } catch (Exception e) {
+            throw new ConfigurationException(e);
+        }
+
+    }
+
+    public static ConfigurationLoader getInstance() {
+
+        if (instance == null) {
+            instance = new ConfigurationLoader();
+        }
+        return instance;
+    }
 }
